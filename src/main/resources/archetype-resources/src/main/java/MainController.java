28ab772665d8d17283aff141303ad3277d833343
aliencode <ip.me@me.com>
2013-07-12 16:59:38 +0800

    first commit!

---
+++ src/main/resources/archetype-resources/src/main/java/MainController.java
@@ -0,0 +1,17 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author aliencode
+ * @date 13-6-7
+ */
+
+@Controller
+public class MainController {
+
+
+}
